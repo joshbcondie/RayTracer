7437070d31ac958e3a769523c9b39dd7d53d8085
Josh <joshbcondie@gmail.com>
2014-11-03 15:32:03 -0700

    Ray-triangle intersection

--- src/Triangle.java
+++ src/Triangle.java
@@ -4,41 +4,51 @@ public class Triangle extends Surface {
 	private Vector3D v2;
 	private Vector3D v3;
 	private Vector3D normal;
-	private double d;
 
 	public Triangle(Vector3D v1, Vector3D v2, Vector3D v3) {
 		this.v1 = v1;
 		this.v2 = v2;
 		this.v3 = v3;
 		normal = v2.subtract(v1).crossProduct(v3.subtract(v2)).normalize();
-		d = -(v1.getX() * normal.getX() + v1.getY() * normal.getY() + v1.getZ()
-				* normal.getZ());
 	}
 
 	@Override
 	public boolean intersects(Ray ray) {
-		double vd = normal.dotProduct(ray.getDirection());
-		if (vd == 0)
+		double a = v1.getX() - v2.getX();
+		double b = v1.getY() - v2.getY();
+		double c = v1.getZ() - v2.getZ();
+		double d = v1.getX() - v3.getX();
+		double e = v1.getY() - v3.getY();
+		double f = v1.getZ() - v3.getZ();
+		double g = ray.getDirection().getX();
+		double h = ray.getDirection().getY();
+		double i = ray.getDirection().getZ();
+		double j = v1.getX() - ray.getOrigin().getX();
+		double k = v1.getY() - ray.getOrigin().getY();
+		double l = v1.getZ() - ray.getOrigin().getZ();
+
+		double M = a * (e * i - h * f) + b * (g * f - d * i) + c
+				* (d * h - e * g);
+		double t = -(f * (a * k - j * b) + e * (j * c - a * l) + d
+				* (b * l - k * c))
+				/ M;
+		if (t < 0) {
 			return false;
-		if (vd > 0) {
-			normal = new Vector3D(-normal.getX(), -normal.getY(),
-					-normal.getZ());
-			d = -(v1.getX() * normal.getX() + v1.getY() * normal.getY() + v1
-					.getZ() * normal.getZ());
-			vd = normal.dotProduct(ray.getDirection());
 		}
-		double v0 = -(normal.dotProduct(ray.getOrigin()) + d);
-		double t = v0 / vd;
-		if (t < 0)
+
+		double gamma = (i * (a * k - j * b) + h * (j * c - a * l) + g
+				* (b * l - k * c))
+				/ M;
+		if (gamma < 0 || gamma > 1) {
 			return false;
-		Vector3D point = new Vector3D(ray.getOrigin().getX()
-				+ ray.getDirection().getX() * t, ray.getOrigin().getY()
-				+ ray.getDirection().getY() * t, ray.getOrigin().getZ()
-				+ ray.getDirection().getZ() * t);
-		if (v2.subtract(v1).dotProduct(point.subtract(v1)) >= 0
-				&& v3.subtract(v2).dotProduct(point.subtract(v2)) >= 0
-				&& v1.subtract(v3).dotProduct(point.subtract(v3)) >= 0)
-			return true;
-		return false;
+		}
+
+		double beta = (j * (e * i - h * f) + k * (g * f - d * i) + l
+				* (d * h - e * g))
+				/ M;
+		if (beta < 0 || beta > 1 - gamma) {
+			return false;
+		}
+		return true;
 	}
 }
